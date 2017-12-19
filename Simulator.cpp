2f03bc2733e8e44eec838f6e84d38d76657429bc
jeferson <jefersonnpn@gmail.com>
2017-12-18 21:44:07 -0300

    adding Simulator header [updates]

--- Simulator.cpp
+++ Simulator.cpp
@@ -1,9 +1,21 @@
 
 #include <iostream>
-#include <random>
-#include <simulator.h>
+#include <string>
+#include "simulator.h"
+
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
+
+  if (argc < 3) {
+    cout << "Usage: <distibution-type> <number-of-repetitions>\n";
+    return 0;
+  }
+
+  string distribution_type = argv[1];
+  int repetitions = atoi(argv[2]);
+
+  cout << distribution_type <<" "<< repetitions << '\n';
+
   return 0;
 }
--- simulator.h
+++ simulator.h
@@ -1,6 +1,8 @@
 
 #include <vector>
 #include <string>
+#include <random>
+#include <ctime>
 
 // distribution types
 #define NORMAL "normal"
@@ -8,9 +10,9 @@
 #define EXPONENTIAL "exponential"
 
 // distribution parameters
-#define MEAN 1
+#define MEAN   1
 #define STDDEV 0.1
-#define ROLLS 1000000
+#define ROLLS  1000000
 
 class Simulator {
   int next = -1;
@@ -20,12 +22,57 @@ class Simulator {
   bool status_server = true;
 
   std::string distribution_type = NORMAL;
+  std::vector<int> queue;
   int average_time_value = 4;
   int duration = 5;
-  int number_of_repetitions = 1;
+  int repetitions = 1;
+  int attend_time_average = 0;
+  int waiting_average = 0;
+  int requests_received = 0;
+  int requests_attended = 0;
+
+private:
+
+    time_t current_time() {
+      return time(NULL);
+    }
 
 public:
-    void run() {
 
+    void run() {
+      // TODO Set paramenters
     }
-}
+
+    void simular() {
+      time_t start;
+      for (int i = 0; i < repetitions; i++) {
+          time(&start); // get current time, same as timer = time(NULL)
+          int queue_requests = 0;
+          int queue_counter = 0;
+          std::string result;
+          std::cout << "repetition #" << i << ": \n";
+
+          scheduling();
+          while (duration > (current_time() - start)) {
+
+            // TODO when it reaches the final moment (time) of the server's use
+
+            // TODO when it reaches the arrival moment in the queue
+
+          }
+      }
+    }
+
+    void scheduling() {
+      // TODO Calculate the next arrival time based on the distribution
+    }
+
+    void allocate_server() {
+      // TODO Change server status and count completed requests
+    }
+
+    void status() {
+      // TODO Print the simulator status
+    }
+
+};
